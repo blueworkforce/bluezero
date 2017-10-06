b567a0194ef80f45e14400c55295eadcb774b4ce
Federico Ferri <federico.ferri.it@gmail.com>
2017-10-06 23:11:22 +0200

    add logger console

---
+++ include/b0/logger/console.h
@@ -0,0 +1,51 @@
+#ifndef CONSOLE_H_INCLUDED
+#define CONSOLE_H_INCLUDED
+
+#include <b0/node.h>
+#include <b0/subscriber.h>
+#include "logger.pb.h"
+
+#include <string>
+#include <boost/thread.hpp>
+#include <boost/format.hpp>
+
+namespace b0
+{
+
+namespace logger
+{
+
+/*!
+ * \brief The logger console node
+ */
+class Console : public Node
+{
+public:
+    /*!
+     * \brief Construct a resolver node
+     */
+    Console();
+
+    /*!
+     * \brief Resolver node destructor
+     */
+    virtual ~Console();
+
+    std::string getName() const {return "console";}
+
+    virtual void onLogMessage(std::string topic, const b0::logger_msgs::LogEntry &entry);
+
+protected:
+    //! Subscriber to "log" topic
+    b0::Subscriber<b0::logger_msgs::LogEntry> sub_;
+
+private:
+    //! A dummy logger to get formatting information from
+    b0::LocalLogger<Console> dummy_logger_;
+};
+
+} // namespace logger
+
+} // namespace b0
+
+#endif // CONSOLE_H_INCLUDED
---
+++ src/b0/logger/console.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <b0/protobufhelpers.hpp>
+#include <b0/logger/logger.h>
+#include <b0/logger/console.h>
+
+namespace b0
+{
+
+namespace logger
+{
+
+Console::Console()
+    : Node("logger_console"),
+      sub_(this, "log", &Console::onLogMessage),
+      dummy_logger_(this)
+{
+}
+
+Console::~Console()
+{
+}
+
+void Console::onLogMessage(std::string topic, const b0::logger_msgs::LogEntry &entry)
+{
+    LocalLogger<Console>::LevelInfo info = dummy_logger_.levelInfo(entry.level());
+    std::cout << info.ansiEscape() << "[" << entry.node_name() << "] " << info.levelStr << ": " << entry.msg() << info.ansiReset() << std::endl;
+}
+
+} // namespace logger
+
+} // namespace b0
+
+int main()
+{
+    b0::logger::Console console;
+    console.init();
+    console.spin();
+    return 0;
+}
+
