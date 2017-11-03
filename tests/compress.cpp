c83cf8e21572e9c7f6682aabe3b4a224a0fb26b6
Federico Ferri <federico.ferri.it@gmail.com>
2017-11-03 13:14:25 +0100

    test multiple payload sizes

--- tests/compress.cpp
+++ tests/compress.cpp
@@ -1,26 +1,37 @@
 #include <b0/utils/compress.hpp>
+#include <sstream>
 #include <iostream>
 #include <iomanip>
 
+std::string generatePayload(size_t size)
+{
+    std::stringstream ss;
+    std::string in = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            "aaaabababababbbbababbbbbbbacccccccccccccccccccccccccccccc";
+    for(size_t i = 0; i < size; i++)
+        ss << in[i % in.size()];
+    return ss.str();
+}
+
 int main(int argc, char **argv)
 {
-    for(int level = -1; level <= 9; level++)
+    size_t size[] = {100, 500, 2000, 8000, 25000, 100000, 1000000, 5000000};
+    for(int j = 0; j < sizeof(size)/sizeof(size[0]); j++)
     {
-        std::string in = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            "aaaabababababbbbababbbbbbbacccccccccccccccccccccccccccccc";
-        std::cout << "in: " << in << std::endl;
-        std::cout << "in size: " << in.size() << std::endl;
-        std::string out = compress(in);
-        std::cout << "out:";
-        for(int i = 0; i < out.size(); i++)
-            std::cout << " " << std::setfill('0') << std::setw(2) << (int)out[i];
-        std::cout << std::endl;
-        std::cout << "out size: " << out.size() << std::endl;
-        if(!(out.size() < in.size())) exit(1);
-        std::string in2 = decompress(out);
-        std::cout << "in2: " << in2 << std::endl;
-        std::cout << "in2 size: " << in2.size() << std::endl;
-        if(in != in2) exit(2);
+        std::cout << "Testing with a payload size of " << size[j] << std::endl;
+        for(int level = -1; level <= 9; level++)
+        {
+            std::cout << "Testing with compression level of " << level << std::endl;
+            std::string in = generatePayload(size[j]);
+            std::cout << "in size: " << in.size() << std::endl;
+            std::string out = compress(in);
+            std::cout << std::endl;
+            std::cout << "out size: " << out.size() << std::endl;
+            if(!(out.size() < in.size())) exit(1);
+            std::string in2 = decompress(out);
+            std::cout << "in2 size: " << in2.size() << std::endl;
+            if(in != in2) exit(2);
+        }
     }
     exit(0);
 }
