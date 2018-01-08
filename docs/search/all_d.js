b496d1a9cad37fdf2fddea01b6e75f1e12bacf79
Federico Ferri <federico.ferri.it@gmail.com>
2018-01-08 23:22:55 +0100

    update docs

--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -4,7 +4,7 @@ var searchData=
   ['pid',['pid',['../classb0_1_1_node.html#acd5d62408e829bb84d1bf099d66d87a6',1,'b0::Node']]],
   ['poll',['poll',['../classb0_1_1socket_1_1_socket.html#a7e5519d393ac5e0c84357c2f7a339002',1,'b0::socket::Socket']]],
   ['pub_5fproxy_5fthread_5f',['pub_proxy_thread_',['../classb0_1_1resolver_1_1_resolver.html#aedab62fe925101f5edbe8a332abe1b60',1,'b0::resolver::Resolver']]],
-  ['publish',['publish',['../classb0_1_1_publisher.html#ae139f7015f9e198e0b99c80480ef71a0',1,'b0::Publisher']]],
+  ['publish',['publish',['../classb0_1_1_publisher.html#ae139f7015f9e198e0b99c80480ef71a0',1,'b0::Publisher::publish(const TMsg &amp;msg)'],['../classb0_1_1_publisher.html#a1a4a9314b5c908507e9aab65633e80a3',1,'b0::Publisher::publish(const std::string &amp;msg)']]],
   ['publisher',['Publisher',['../classb0_1_1_publisher.html',1,'b0::Publisher&lt; TMsg &gt;'],['../classb0_1_1_publisher.html#a7988395f2a4855ab19ad06f1e33d1492',1,'b0::Publisher::Publisher()']]],
   ['publisher_3c_20b0_3a_3aresolver_5fmsgs_3a_3agraph_20_3e',['Publisher&lt; b0::resolver_msgs::Graph &gt;',['../classb0_1_1_publisher.html',1,'b0']]],
   ['pubproxy',['pubProxy',['../classb0_1_1resolver_1_1_resolver.html#a9e74f9f2c7827c44766f450be0a1bfc1',1,'b0::resolver::Resolver']]]
