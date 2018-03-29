75d3ea7235217ecd9c843674a5793bfa608ed451
Federico Ferri <federico.ferri.it@gmail.com>
2018-03-29 13:45:49 +0200

    some fixes

--- bindings/bluezero.h
+++ bindings/bluezero.h
@@ -23,10 +23,51 @@ public:
         node_->cleanup();
     }
 
+    void shutdown()
+    {
+        node_->shutdown();
+    }
+
+    bool shutdownRequested()
+    {
+        return node_->shutdownRequested();
+    }
+
+    void spin()
+    {
+        node_->spin();
+    }
+
+    void spinOnce()
+    {
+        node_->spinOnce();
+    }
+
+    std::string getName()
+    {
+        return node_->getName();
+    }
+
+    int getState()
+    {
+        return node_->getState();
+    }
+
+    int64_t hardwareTimeUSec()
+    {
+        return node_->hardwareTimeUSec();
+    }
+
+    int64_t timeUSec()
+    {
+        return node_->timeUSec();
+    }
+
 private:
     b0::Node *node_;
 
     friend class Publisher;
+    friend class Subscriber;
 };
 
 class Publisher
@@ -50,4 +91,30 @@ public:
 private:
     b0::Publisher *pub_;
 };
+
+class SubscriberCallback
+{
+public:
+    void onMessage(const std::string &message) {}
+};
+
+class Subscriber
+{
+public:
+    Subscriber(Node &node, const std::string &topic, SubscriberCallback *cb)
+        : cb_(cb),
+          sub_(new b0::Subscriber(node.node_, topic, [=](const std::string &m) {cb_->onMessage(m);}))
+    {
+    }
+
+    ~Subscriber()
+    {
+        delete sub_;
+    }
+
+private:
+    SubscriberCallback *cb_;
+    b0::Subscriber *sub_;
+};
+
 
