9d25e519eb0f21ce56c8cdd671cf74c3eeb3b171
Federico Ferri <federico.ferri.it@gmail.com>
2018-07-20 09:17:41 +0200

    remove b0/logger/console.h header

--- src/b0/logger/console.cpp
+++ src/b0/logger/console.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <map>
 
-#include <boost/asio.hpp>
-#include <boost/thread.hpp>
-#include <boost/format.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-
-#include <b0/publisher.h>
+#include <b0/node.h>
+#include <b0/subscriber.h>
 #include <b0/logger/logger.h>
-#include <b0/logger/console.h>
+#include <b0/message/log_entry.h>
 
 namespace b0
 {
@@ -19,22 +12,38 @@ namespace b0
 namespace logger
 {
 
-Console::Console()
-    : Node("logger_console"),
-      sub_(this, "log", &Console::onLogMessage, this),
-      dummy_logger_(this)
+class Console : public Node
 {
-}
+public:
+    Console()
+        : Node("logger_console"),
+          sub_(this, "log", &Console::onLogMessage, this),
+          dummy_logger_(this)
+    {
+    }
 
-Console::~Console()
-{
-}
+    ~Console()
+    {
+    }
 
-void Console::onLogMessage(const b0::message::LogEntry &entry)
-{
-    LocalLogger::LevelInfo info = dummy_logger_.levelInfo(entry.level);
-    std::cout << info.ansiEscape() << "[" << entry.node_name << "] " << info.levelStr << ": " << entry.message << info.ansiReset() << std::endl;
-}
+    std::string getName() const
+    {
+        return "console";
+    }
+
+    void onLogMessage(const b0::message::LogEntry &entry)
+    {
+        LocalLogger::LevelInfo info = dummy_logger_.levelInfo(entry.level);
+        std::cout << info.ansiEscape() << "[" << entry.node_name << "] " << info.levelStr << ": " << entry.message << info.ansiReset() << std::endl;
+    }
+
+protected:
+    b0::Subscriber sub_;
+
+private:
+    // A dummy logger to get formatting information from:
+    b0::logger::LocalLogger dummy_logger_;
+};
 
 } // namespace logger
 
