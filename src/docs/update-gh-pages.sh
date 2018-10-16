75da82c26f1f0eb35fb54ee32eb61cc6b88faa0d
Federico Ferri <federico.ferri.it@gmail.com>
2018-10-16 17:47:07 +0200

    auto-determine branch-name

--- src/docs/update-gh-pages.sh
+++ src/docs/update-gh-pages.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-DEST=v2
+GIT_CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 set -x
 set -e
 git checkout gh-pages
 git fetch --all
 git reset --hard origin/gh-pages
-rm -rf ${DEST} && mkdir ${DEST}
-cp -r docs/* ${DEST}/
+rm -rf $GIT_CURRENT_BRANCH && mkdir $GIT_CURRENT_BRANCH
+cp -r docs/* $GIT_CURRENT_BRANCH/
 git add .
 git commit --amend -m 'update docs'
 git push -f
-git checkout devel
+git checkout $GIT_CURRENT_BRANCH
