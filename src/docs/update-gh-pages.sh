253927d2b2eec23db21ff8816d7d23f281985a84
Federico Ferri <federico.ferri.it@gmail.com>
2018-10-16 16:39:37 +0200

    make update-gh-pages.sh work w/ multiple versions

--- src/docs/update-gh-pages.sh
+++ src/docs/update-gh-pages.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+DEST=v1
 set -x
 set -e
 git checkout gh-pages
-rm *.html *.png *.css *.js ; rm -rf search
-cp -r docs/* .
+git fetch --all
+git reset --hard origin/gh-pages
+rm -rf ${DEST} && mkdir ${DEST}
+cp -r docs/* ${DEST}/
 git add .
 git commit --amend -m 'update docs'
 git push -f
