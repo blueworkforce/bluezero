74e834982c3ed9256efd18d1f854471a4c44ab0c
Federico Ferri <federico.ferri.it@gmail.com>
2018-11-28 11:38:10 +0100

    file argument can appear any number of times

--- examples/cmdline_args/args.cpp
+++ examples/cmdline_args/args.cpp
@@ -12,11 +12,13 @@ int main(int argc, char **argv)
 {
     b0::addOptionString("fancy-name,n", "a string arg");
     b0::addOptionInt("lucky-number,x", "an int arg with default", nullptr, false, 23);
-    b0::addOptionString("file,f", "file arg", nullptr, true);
-    b0::setPositionalOption("file");
+    b0::addOptionStringVector("file,f", "file arg", nullptr, true);
+    b0::setPositionalOption("file", -1);
     b0::init(argc, argv);
 
-    cout << "file = " << b0::getOptionString("file") << endl;
+    for(auto f : b0::getOptionStringVector("file"))
+        cout << "file = " << f << endl;
+
     if(b0::hasOption("lucky-number"))
     {
         int x = b0::getOptionInt("lucky-number");
