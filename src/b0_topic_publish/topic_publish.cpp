74a0139b6828e52697a8943a37eebcf89fc27de9
Federico Ferri <federico.ferri.it@gmail.com>
2018-11-29 11:02:45 +0100

    use spin callback and publish at node's spin rate

--- src/b0_topic_publish/topic_publish.cpp
+++ src/b0_topic_publish/topic_publish.cpp
@@ -10,11 +10,10 @@
 int main(int argc, char **argv)
 {
     std::string node_name = "b0_topic_publish", topic_name = "", content_type = "";
-    double rate = 0.0;
     b0::addOptionString("node-name,n", "name of node", &node_name);
     b0::addOptionString("topic-name,t", "name of topic", &topic_name);
     b0::addOptionString("content-type,c", "content type", &content_type);
-    b0::addOptionDouble("rate,r", "publish rate (0 means one-shot)", &rate);
+    b0::addOption("one-shot,1", "publish once and exit");
     b0::setPositionalOption("topic-name");
     b0::init(argc, argv);
 
@@ -26,12 +25,11 @@ int main(int argc, char **argv)
     b0::Node node(node_name);
     b0::Publisher pub(&node, topic_name);
     node.init();
-    while(!node.shutdownRequested())
-    {
-        pub.publish(content_type, payload);
-        if(rate == 0.0) break;
-        boost::this_thread::sleep_for(boost::chrono::microseconds{long(1000000 / rate)});
-    }
+    node.spin([&]() {
+        pub.publish(payload, content_type);
+        if(b0::hasOption("one-shot"))
+            node.shutdown();
+    });
     node.cleanup();
     return 0;
 }
