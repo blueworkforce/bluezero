eaa4b01f49c70d6557649d0568c7b7341dc582df
Federico Ferri <federico.ferri.it@gmail.com>
2018-11-29 15:08:30 +0100

    move client to main thread; test success & failure

--- tests/c_clisrv_timeout.c
+++ tests/c_clisrv_timeout.c
@@ -8,9 +8,10 @@
 
 pid_t resolver_pid = -1;
 pid_t server_pid = -1;
-pid_t client_pid = -1;
 const char *service_name = "test_service";
 int server_wait = 0;
+int expect_failure = 0;
+int read_success = -1;
 
 void start_resolver()
 {
@@ -88,51 +89,35 @@ void kill_server(int sig)
 
 void start_client()
 {
-    client_pid = fork();
+    b0_node *client_node = b0_node_new("client");
+    b0_service_client *cli = b0_service_client_new(client_node, service_name);
+    b0_service_client_set_option(cli, B0_SOCK_OPT_READTIMEOUT, 2000);
+    b0_node_init(client_node);
 
-    if(client_pid == -1)
+    const char *req = "hello";
+    printf("client: Sending: %s\n", req);
+    char *rep;
+    size_t rep_sz;
+    rep = b0_service_client_call(cli, req, strlen(req) + 1, &rep_sz);
+    read_success = rep ? 1 : 0;
+    if(rep)
     {
-        fprintf(stderr, "error: start_client: fork failed\n");
-        exit(1);
+        printf("client: Received: %s\n", rep);
+        b0_buffer_delete(rep);
     }
-    else if(client_pid == 0)
+    else
     {
-        b0_node *client_node = b0_node_new("client");
-        b0_service_client *cli = b0_service_client_new(client_node, service_name);
-        b0_service_client_set_option(cli, B0_SOCK_OPT_READTIMEOUT, 2000);
-        b0_node_init(client_node);
-
-        const char *req = "hello";
-        printf("client: Sending: %s\n", req);
-        char *rep;
-        size_t rep_sz;
-        rep = b0_service_client_call(cli, req, strlen(req) + 1, &rep_sz);
-        if(rep)
-        {
-            printf("client: Received: %s\n", rep);
-            b0_buffer_delete(rep);
-        }
-        else
-        {
-            printf("client: Service call failed (timeout?)\n");
-        }
-
-        b0_node_cleanup(client_node);
-        b0_service_client_delete(cli);
-        b0_node_delete(client_node);
+        printf("client: Service call failed (timeout?)\n");
     }
-}
 
-void kill_client(int sig)
-{
-    if(client_pid > 0)
-        kill(client_pid, SIGKILL);
+    b0_node_cleanup(client_node);
+    b0_service_client_delete(cli);
+    b0_node_delete(client_node);
 }
 
 void kill_all(int sig)
 {
     kill_server(sig);
-    kill_client(sig);
     kill_resolver(sig);
 }
 
@@ -143,8 +128,10 @@ int main(int argc, char **argv)
     signal(SIGABRT, (void (*)(int))kill_all);
 
     b0_add_option_int("server-wait,w", "server wait in seconds", 1, 0);
+    b0_add_option_int("expect-failure,f", "test shoudl fail", 1, 0);
     b0_init(&argc, argv);
     b0_get_option_int("server-wait", &server_wait);
+    b0_get_option_int("expect-failure", &expect_failure);
 
     start_resolver();
 
@@ -154,7 +141,6 @@ int main(int argc, char **argv)
     start_client();
 
     int status;
-    waitpid(client_pid, &status, 0);
 
     kill_server(SIGTERM);
     waitpid(server_pid, &status, 0);
@@ -162,5 +148,7 @@ int main(int argc, char **argv)
     kill_resolver(SIGTERM);
     waitpid(resolver_pid, &status, 0);
 
-    return 0;
+    if(read_success == -1) return 2;
+
+    return read_success == expect_failure;
 }
